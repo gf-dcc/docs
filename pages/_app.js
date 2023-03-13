ffb5c210ce7c4427ecd0228ff2135d4b7bc47a9b
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-03-13 10:04:17 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import 'nextra-theme-docs/style.css'
-export default function Nextra({ Component, pageProps }) {
+//import 'nextra-theme-docs/style.css'
+//export default function Nextra({ Component, pageProps }) {
+//return <Component {...pageProps} /> }
+
+
+import '../styles.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
