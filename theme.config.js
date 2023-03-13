21fa6995af58a02ed145ed5dca891342ff219e76
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-03-13 11:22:45 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -15,29 +15,31 @@ function App() {
   );
 }
 
+export default App;
+
 export default {
-  {App;
   projectLink: 'https://github.com/gf-dcc/docs', // GitHub link in the navbar
   docsRepositoryBase: 'https://github.com/gf-dcc/docs/blob/main', // base URL for the docs repository
   titleSuffix: ' – Nextra',
   nextLinks: true,
   prevLinks: true,
   search: true,
-  //customSearch: <docsearch/>,
-  darkMode: false,
-  footer: false,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
   footerText: ``,
   footerEditLink: `Edit this page on GitHub`,
-  logo: <span> </span>,
+  logo: (
+    <>
+      <svg>...</svg>
+      <span></span>
+    </>
+  ),
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Nextra: the next docs builder" />
       <meta name="og:title" content="Nextra: the next docs builder" />
     </>
-  )
+  ),
 }
-}
-
-
-
