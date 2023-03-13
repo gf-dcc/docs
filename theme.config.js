883e0688f8e37eb80768c7dfc9f58387247846b7
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-03-13 11:36:35 -0400

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,19 +1,26 @@
-//import docsearch from './components/docsearch'
-
-<script src="https://cdn.jsdelivr.net/npm/@docsearch/react@3"></script>
-
-import { DocSearch } from '@docsearch/react';
-
-import '@docsearch/css';
-
-function App() {
-  return (
-    <DocSearch
-      appId="R2IYF7ETH7"
-      indexName="599cec31baffa4868cae4e79f180729b"
-      apiKey="docsearch"
-    />
-  );
+export default {
+  projectLink: 'https://github.com/gf-dcc/docs', // GitHub link in the navbar
+  docsRepositoryBase: 'https://github.com/gf-dcc/docs/blob/main', // base URL for the docs repository
+  titleSuffix: ' – Nextra',
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null, // customizable, you can use algolia for example
+  darkMode: true,
+  footer: true,
+  footerText: ``,
+  footerEditLink: `Edit this page on GitHub`,
+  logo: (
+    <>
+      <svg>...</svg>
+      <span></span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Nextra: the next docs builder" />
+      <meta name="og:title" content="Nextra: the next docs builder" />
+    </>
+  ),
 }
-
-export default App;
