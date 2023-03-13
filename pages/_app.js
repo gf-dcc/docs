15dbeab225ca3595f4b5b2ad9eb21a13b52c8e14
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-03-13 10:08:20 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@
 //return <Component {...pageProps} /> }
 
 
-import '../styles.css'
+import 'https://github.com/gf-dcc/docs/blob/2c6c39a5b0975be8809d638595db8bbabb9f0c78/pages/styles.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
