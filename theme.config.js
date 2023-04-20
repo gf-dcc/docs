536198ab5ca583bfcc2b12197c4267501e65e2f8
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-04-20 16:42:50 -0400

    Update theme.config.js
    this just test/practice

--- theme.config.js
+++ theme.config.js
@@ -1,44 +1,18 @@
-export default {
-  projectLink: 'https://github.com/gf-dcc/docs', // GitHub link in the navbar
-  docsRepositoryBase: 'https://github.com/gf-dcc/docs/blob/main', // base URL for the docs repository
-  titleSuffix: ' – Nextra',
-  nextLinks: true,
-  prevLinks: true,
-  darkMode: true,
-  footer: true,
-  footerText: ``,
-  footerEditLink: `Edit this page on GitHub`,
-  navigation: {
-    prev: true,
-    next: true
-  }
-  logo: (
-    <>
-      <svg>...</svg>
-      <span></span>
-    </>
-  ),
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="Nextra: the next docs builder" />
-      <meta name="og:title" content="Nextra: the next docs builder" />
-    </>
-  ),
-  banner: {
-    key: 'beta-announcement',
-    text: <a href="https://nextra.site" target="_blank">
-      Welcome! Our docs are under construction!
-    </a>,
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
   },
   chat: {
-    link: 'https://join.slack.com/t/gfbrcaprecanceratlas/shared_invite/zt-1s4ahne9e-7wlj1Db02KZtJ6bJxlqnfA',
-    icon: <svg width="50" height="50" viewBox="0 0 248 204" xmlns="http://www.w3.org/2000/svg">
-    <path d="M27.2 80c0 7.3-5.9 13.2-13.2 13.2C6.7 93.2.8 87.3.8 80c0-7.3 5.9-13.2 13.2-13.2h13.2V80zm6.6 0c0-7.3 5.9-13.2 13.2-13.2 7.3 0 13.2 5.9 13.2 13.2v33c0 7.3-5.9 13.2-13.2 13.2-7.3 0-13.2-5.9-13.2-13.2V80z" fill="#E01E5A"/>
-    <path d="M47 27c-7.3 0-13.2-5.9-13.2-13.2C33.8 6.5 39.7.6 47 .6c7.3 0 13.2 5.9 13.2 13.2V27H47zm0 6.7c7.3 0 13.2 5.9 13.2 13.2 0 7.3-5.9 13.2-13.2 13.2H13.9C6.6 60.1.7 54.2.7 46.9c0-7.3 5.9-13.2 13.2-13.2H47z" fill="#36C5F0"/>
-    <path d="M99.9 46.9c0-7.3 5.9-13.2 13.2-13.2 7.3 0 13.2 5.9 13.2 13.2 0 7.3-5.9 13.2-13.2 13.2H99.9V46.9zm-6.6 0c0 7.3-5.9 13.2-13.2 13.2-7.3 0-13.2-5.9-13.2-13.2V13.8C66.9 6.5 72.8.6 80.1.6c7.3 0 13.2 5.9 13.2 13.2v33.1z" fill="#2EB67D"/>
-    <path d="M80.1 99.8c7.3 0 13.2 5.9 13.2 13.2 0 7.3-5.9 13.2-13.2 13.2-7.3 0-13.2-5.9-13.2-13.2V99.8h13.2zm0-6.6c-7.3 0-13.2-5.9-13.2-13.2 0-7.3 5.9-13.2 13.2-13.2h33.1c7.3 0 13.2 5.9 13.2 13.2 0 7.3-5.9 13.2-13.2 13.2H80.1z" fill="#ECB22E"/>
-  </svg>,
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
   },
 }
 
+export default config
